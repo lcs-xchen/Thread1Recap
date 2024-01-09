9271b38d5f21877c544298e951f2fe58c8e52304
Cindy Chen <xchen@lcs.on.ca>
2024-01-09 14:59:39 -0500

    Completed all of the stored properties for the 2 dogs with added properties of mass in Kg and height in metres.

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Dog {
+    let breed: String
+    let color: String
+    let lengthInCentimetres: Double
+    let massInKg: Double
+    var heightInMetres: Double
+}
+
+var dogAtLeft = Dog(
+    breed: "English Bulldog",
+    color: "Light brown, some white.",
+    lengthInCentimetres: 60,
+    massInKg: 20.5,
+    heightInMetres: 0.457
+)
+
+var dogInMiddle = Dog(
+    breed: "German Shepherd",
+    color: "Black and dark brown.",
+    lengthInCentimetres: 100,
+    massInKg: 36,
+    heightInMetres: 0.753
+)
