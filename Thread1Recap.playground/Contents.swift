cb2476a605c506e145f70ef9bbbc72d32f39eab9
Cindy Chen <xchen@lcs.on.ca>
2024-01-09 20:52:11 -0500

    Questions 3, 4,  and 5

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,11 +1,17 @@
 import Cocoa
 
 struct Dog {
+    // MARK: Stored properties
     let breed: String
     let color: String
     let lengthInCentimetres: Double
     let massInKg: Double
     var heightInMetres: Double
+    
+    // MARK: Computed properties
+    var heightInCentimetres: Double {
+          return heightInMetres * 100
+      }
 }
 
 var dogAtLeft = Dog(
@@ -23,3 +29,20 @@ var dogInMiddle = Dog(
     massInKg: 36,
     heightInMetres: 0.753
 )
+
+dogAtLeft.heightInCentimetres
+dogInMiddle.heightInCentimetres
+
+
+
+
+
+
+
+
+
+
+
+
+
+
